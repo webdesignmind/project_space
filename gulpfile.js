932e7e9963ec793bbce2a9780311ed404a73878d
webdesignmind <axenoffdenis@yandex.ru>
2021-06-21 23:28:52 +0300

    added some files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require("gulp");
+var bs = require("browser-sync");
 
 gulp.task("hello", function () {
   console.log;
+});
+
+gulp.task("browser-sync", function () {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
 });
