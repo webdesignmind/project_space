8628294a82bb75fcd866251998a591cc945b1b0b
webdesignmind <axenoffdenis@yandex.ru>
2021-07-07 21:37:18 +0300

    added some changes in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
 var gulp = require("gulp");
 var bs = require("browser-sync");
+var sass = require("gulp-sass");
 
-gulp.task("hello", function () {
-  console.log;
-});
-
-gulp.task("browser-sync", function () {
-  browserSync.init({
-    server: {
-      baseDir: "./",
-    },
+gulp.task("bs", function () {
+  bs.init({
+    server: "./src",
   });
+
+  gulp.watch("src/sass/*.sass", ["sass"]);
+  gulp.watch("src/*.html").on("change", bs.reload);
 });
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task("sass", function () {
+  return gulp
+    .src("src/sass/*.sass")
+    .pipe(sass())
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("default", ["serve"]);
